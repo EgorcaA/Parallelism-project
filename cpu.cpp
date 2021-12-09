dd57b886c32177ca951d945e5d8f4b25066ed325
Egor <55582598+EgorcaA@users.noreply.github.com>
2021-12-10 00:40:16 +0300

    Add files via upload

---
+++ cpu.cpp
@@ -0,0 +1,195 @@
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility> // std::pair
+#include <stdexcept> // std::runtime_error
+#include <sstream> // std::stringstream
+#include <iostream>
+#include <math.h> 
+#include <cstring>
+#include <omp.h>
+#include <chrono>
+
+
+std::vector<std::pair<std::string, std::vector<double>>> read_csv(std::string filename){
+    // Reads a CSV file into a vector of <string, vector<int>> pairs where
+    // each pair represents <column name, column values>
+
+    // Create a vector of <string, int vector> pairs to store the result
+    std::vector<std::pair<std::string, std::vector<double>>> result;
+
+    // Create an input filestream
+    std::ifstream myFile(filename);
+
+    // Make sure the file is open
+    if(!myFile.is_open()) throw std::runtime_error("Could not open file");
+
+    // Helper vars
+    std::string line, colname;
+    double val;
+
+    // Read the column names
+    if(myFile.good())
+    {
+        // Extract the first line in the file
+        std::getline(myFile, line);
+
+        // Create a stringstream from line
+        std::stringstream ss(line);
+
+        // Extract each column name
+        while(std::getline(ss, colname, ',')){
+            
+            // Initialize and add <colname, int vector> pairs to result
+            result.push_back({colname, std::vector<double> {}});
+        }
+    }
+
+    // Read data, line by line
+    while(std::getline(myFile, line))
+    {
+        // Create a stringstream of the current line
+        std::stringstream ss(line);
+        
+        // Keep track of the current column index
+        int colIdx = 0;
+        
+        // Extract each integer
+        while(ss >> val){
+            
+            // Add the current integer to the 'colIdx' column's values vector
+            result.at(colIdx).second.push_back(val);
+            
+            // If the next token is a comma, ignore it and move on
+            if(ss.peek() == ',') ss.ignore();
+            
+            // Increment the column index
+            colIdx++;
+        }
+    }
+
+    // Close file
+    myFile.close();
+
+    return result;
+}
+
+void print_map(double **rho, int N) {
+        
+        std::ofstream fout;
+        fout.open("out.csv", std::fstream::out); //std::ios::app 
+        
+        if (!fout.is_open()) std::cout << "ошибка открытия out";
+        else{
+            for (int i = 0; i < N; ++i) fout<< "name,";
+            fout<<std::endl;
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < N; ++j)
+                {
+                    std::cout.precision(17);
+                    fout << std::fixed << rho[i][j] << ',';
+                }
+                fout<<std::endl;
+            }
+        }
+        fout.close();
+    }
+
+
+void openmp(double **rho, std::vector<std::pair<std::string, std::vector<double>>> data, double *x_grid,
+             double *z_grid, int N, int len_data, double alpha, int ll, int n_threads){
+    
+    // double val = 0.;
+    auto start = std::chrono::high_resolution_clock::now();
+    // int n_threads = 3;
+    omp_set_num_threads(n_threads);
+    #pragma omp parallel for schedule(dynamic)
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+        {
+            rho[i][j] = 0.;
+
+            for (int k = 0; k < len_data; ++k)
+            {
+                rho[i][j] += data[3].second[k] * exp( -alpha*sqrt(pow(x_grid[i] - data[0].second[k], 2.) +
+                                                                  pow(0         - data[1].second[k], 2.) + 
+                                                                  pow(z_grid[j] - data[2].second[k], 2.) )  );
+            }
+        }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> durwrite = end - start;
+
+
+    std::cout << "alpha  " <<ll << " N   " << N << "   time    " << durwrite.count() << std::endl;
+
+    std::ofstream fout;
+    fout.open("logs.txt", std::fstream::app); //std::ios::app 
+    if (!fout.is_open()) std::cout << "ошибка открытия logs";
+    else fout<< "openmp num threads  "<< n_threads  << "  alpha  " <<ll << " N   " << N << "   time    "<< durwrite.count() << std::endl;
+    fout.close();
+
+    print_map(rho, N);
+}
+
+int main(int argc, char const *argv[]) {
+    int n_threads = 3;
+    int N = 50;
+    double ll = 1.;
+
+    for (int i=1;i<argc;i++) {
+        if (!strcmp(argv[i],"-N")) {
+            N=atoi(argv[++i]);
+            
+        }
+        else if (!strcmp(argv[i],"-ll")) {
+            ll=(double)atof(argv[++i]);
+        }
+        else if (!strcmp(argv[i],"-p")) {
+            n_threads=atoi(argv[++i]);
+        }
+    }
+
+    std::cout<< "N   " << N << std::endl;
+    std::cout<< "ll   " << ll << std::endl;
+
+
+    std::vector<std::pair<std::string, std::vector<double>>> data = read_csv("xyz270.csv");
+
+    int len_data = data[0].second.size();
+    std::cout<< "Num of str " << len_data << std::endl;
+
+    
+    double R = 3526229.;
+    double alpha = R/10000000000000000.*ll;
+
+    double *x_grid = new double [N];
+    for (int i = 0; i < N; ++i) x_grid[i] = -R + (double)i/(N - 1)*2*R;
+
+    double *z_grid = new double [N];
+    for (int i = 0; i < N; ++i) z_grid[i] = -R + (double)i/(N - 1)*2*R;
+
+    double **rho =  new double* [N];
+    for (int i = 0; i < N; ++i) rho[i] = new double [N];
+    
+    // double val = 0.;
+
+
+
+
+    
+    openmp(rho, data, x_grid, z_grid, N, len_data, alpha, ll, n_threads);
+
+    
+
+
+    // std::cout<<rho[0][0];
+    
+    delete[] z_grid, x_grid;
+    for (int i = 0; i < N; ++i) delete[] rho[i];
+    
+    return 0;
+}
+
+
---
+++ gpu.cu
@@ -0,0 +1,233 @@
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility> // std::pair
+#include <stdexcept> // std::runtime_error
+#include <sstream> // std::stringstream
+#include <iostream>
+#include <math.h> 
+#include <cstring>
+#include <omp.h>
+#include <chrono>
+
+#define THREADS_PER_BLOCK 512
+
+#define CUDA_DEBUG
+
+#ifdef CUDA_DEBUG
+
+#define CUDA_CHECK_ERROR(err)           \
+if (err != cudaSuccess) {          \
+printf("Cuda error: %s\n", cudaGetErrorString(err));    \
+printf("Error in file: %s, line: %i\n", __FILE__, __LINE__);  \
+}                 \
+
+#else
+
+#define CUDA_CHECK_ERROR(err)
+
+#endif
+
+
+std::vector<std::pair<std::string, std::vector<double>>> read_csv(std::string filename){
+    // Reads a CSV file into a vector of <string, vector<int>> pairs where
+    // each pair represents <column name, column values>
+
+    // Create a vector of <string, int vector> pairs to store the result
+    std::vector<std::pair<std::string, std::vector<double>>> result;
+
+    // Create an input filestream
+    std::ifstream myFile(filename);
+
+    // Make sure the file is open
+    if(!myFile.is_open()) throw std::runtime_error("Could not open file");
+
+    // Helper vars
+    std::string line, colname;
+    double val;
+
+    // Read the column names
+    if(myFile.good())
+    {
+        // Extract the first line in the file
+        std::getline(myFile, line);
+
+        // Create a stringstream from line
+        std::stringstream ss(line);
+
+        // Extract each column name
+        while(std::getline(ss, colname, ',')){
+            
+            // Initialize and add <colname, int vector> pairs to result
+            result.push_back({colname, std::vector<double> {}});
+        }
+    }
+
+    // Read data, line by line
+    while(std::getline(myFile, line))
+    {
+        // Create a stringstream of the current line
+        std::stringstream ss(line);
+        
+        // Keep track of the current column index
+        int colIdx = 0;
+        
+        // Extract each integer
+        while(ss >> val){
+            
+            // Add the current integer to the 'colIdx' column's values vector
+            result.at(colIdx).second.push_back(val);
+            
+            // If the next token is a comma, ignore it and move on
+            if(ss.peek() == ',') ss.ignore();
+            
+            // Increment the column index
+            colIdx++;
+        }
+    }
+
+    // Close file
+    myFile.close();
+
+    return result;
+}
+
+void print_map(double *rho, int N) {
+        
+        std::ofstream fout;
+        fout.open("out.csv", std::fstream::out); //std::ios::app 
+        
+        if (!fout.is_open()) std::cout << "ошибка открытия out";
+        else{
+            for (int i = 0; i < N; ++i) fout<< "name,";
+            fout<<std::endl;
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < N; ++j)
+                {
+                    std::cout.precision(17);
+                    fout << std::fixed << rho[i*N + j] << ',';
+                }
+                fout<<std::endl;
+            }
+        }
+        fout.close();
+    }
+
+
+__global__  void gpu(double *Dx_grid, double *Dz_grid, double *Drho, double *Dx_data, double *Dy_data, double *Dz_data, 
+				double *Ddensity_data, int N, int len_data, double alpha, double ll){
+    
+    int ident = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = ident/N;
+    int j = ident%N;
+    Drho[ident] = 0.;
+    for (int k = 0; k < len_data; ++k)
+    {
+        Drho[ident] += Ddensity_data[k] * exp( -alpha*sqrt(pow(Dx_grid[i] - Dx_data[k], 2.) +
+                                                          pow(0          - Dy_data[k], 2.) + 
+                                                          pow(Dz_grid[j] - Dz_data[k], 2.) )  );
+
+    }
+        
+}
+
+
+
+int main(int argc, char const *argv[]) {
+    int N = 50;
+    double ll = 1.;
+
+    for (int i=1;i<argc;i++) {
+        if (!strcmp(argv[i],"-N")) {
+            N=atoi(argv[++i]);
+            
+        }
+        else if (!strcmp(argv[i],"-ll")) {
+            ll=(double)atof(argv[++i]);
+        }
+    }
+
+    std::cout<< "N   " << N << std::endl;
+    std::cout<< "ll   " << ll << std::endl;
+
+
+    std::vector<std::pair<std::string, std::vector<double>>> data = read_csv("xyz270.csv");
+
+    int len_data = data[0].second.size();
+    std::cout<< "Num of str " << len_data << std::endl;
+
+    
+    double *x_data = new double [len_data];
+    for (int i = 0; i < len_data; ++i) x_data[i] = data[0].second[i];
+    double *y_data = new double [len_data];
+    for (int i = 0; i < len_data; ++i) y_data[i] = data[1].second[i];
+    double *z_data = new double [len_data];
+    for (int i = 0; i < len_data; ++i) z_data[i] = data[2].second[i];
+    double *density_data = new double [len_data];
+    for (int i = 0; i < len_data; ++i) density_data[i] = data[3].second[i];
+
+    double R = 3526229.;
+    double alpha = R/10000000000000000.*ll;
+
+    double *x_grid = new double [N];
+    for (int i = 0; i < N; ++i) x_grid[i] = -R + (double)i/(N - 1)*2*R;
+
+    double *z_grid = new double [N];
+    for (int i = 0; i < N; ++i) z_grid[i] = -R + (double)i/(N - 1)*2*R;
+
+    double *rho =  new double [N*N];
+    for (int i = 0; i < N*N; ++i) rho[i] = 0.;
+    
+ 
+	double *Dz_grid, *Dx_grid, *Drho, *Dx_data, *Dy_data, *Dz_data, *Ddensity_data;
+	
+    cudaMalloc((void**)&Dx_grid, sizeof(double)*N);	
+	cudaMalloc((void**)&Dz_grid, sizeof(double)*N);
+	cudaMalloc((void**)&Drho, sizeof(double)*N*N);	
+	cudaMalloc((void**)&Dx_data, sizeof(double)*len_data);
+	cudaMalloc((void**)&Dy_data, sizeof(double)*len_data);
+	cudaMalloc((void**)&Dz_data, sizeof(double)*len_data);	
+	cudaMalloc((void**)&Ddensity_data, sizeof(double)*len_data);
+
+
+	CUDA_CHECK_ERROR(cudaMemcpy(Dx_grid, x_grid, sizeof(double)*N, cudaMemcpyHostToDevice));
+	CUDA_CHECK_ERROR(cudaMemcpy(Dz_grid, z_grid, sizeof(double)*N, cudaMemcpyHostToDevice));
+	CUDA_CHECK_ERROR(cudaMemcpy(Drho, rho, sizeof(double)*N*N, cudaMemcpyHostToDevice));
+	CUDA_CHECK_ERROR(cudaMemcpy(Dx_data, x_data, sizeof(double)*len_data, cudaMemcpyHostToDevice));
+	CUDA_CHECK_ERROR(cudaMemcpy(Dy_data, y_data, sizeof(double)*len_data, cudaMemcpyHostToDevice));
+	CUDA_CHECK_ERROR(cudaMemcpy(Dz_data, z_data, sizeof(double)*len_data, cudaMemcpyHostToDevice));
+	CUDA_CHECK_ERROR(cudaMemcpy(Ddensity_data, density_data, sizeof(double)*len_data, cudaMemcpyHostToDevice));
+
+
+	float timerValueGPU;
+	cudaEvent_t start, stop;
+    cudaEventCreate ( &start);
+    cudaEventCreate ( &stop);
+	cudaEventRecord(start, 0);
+    gpu <<< N*N/THREADS_PER_BLOCK, THREADS_PER_BLOCK >>>(Dx_grid, Dz_grid, Drho, Dx_data, Dy_data, Dz_data, Ddensity_data, N, len_data, alpha, ll);
+    cudaEventRecord(stop,0);
+   	cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&timerValueGPU, start, stop);
+
+
+    CUDA_CHECK_ERROR(cudaMemcpy(rho, Drho, sizeof(double)*N*N, cudaMemcpyDeviceToHost));
+    
+    printf ("\n GPU calculation time %f msec\n", timerValueGPU);
+    std::cout << "alpha  " <<ll << " N   " << N << "   time    " << timerValueGPU << std::endl;
+
+    std::ofstream fout;
+    fout.open("logs.txt", std::fstream::app); //std::ios::app 
+    if (!fout.is_open()) std::cout << "ошибка открытия logs";
+    else fout<< "gpu "  << "  alpha  " <<ll << " N   " << N << "   time    "<< timerValueGPU << std::endl;
+    fout.close();
+
+    print_map(rho, N);
+    // std::cout<<rho[0][0];
+    
+    delete[] z_grid, x_grid, rho, x_data, y_data, z_data, density_data;
+    
+    return 0;
+}
+
+
